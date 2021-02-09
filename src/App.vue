09109aa8932e9a54d1207ead9a8a1cf184517466
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-02-09 21:52:20 +0900

    message

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="header">
-    <Header @getChildText="showChildText"></Header>
-    <p>Address：{{text}}</p>
+    <input type="text" maxlength="7" v-model="childText" />
+    <button @click="sendParent">住所自動入力</button>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
+import axios from "axios";
 export default {
   data() {
     return {
-      text: ""
-    };
-  },
-  methods: {
-    showChildText(childText) {
-      this.text = childText;
+      childText: "",
+      // データ内で定義する必要があります
+      Address ：　["addressData"];
     }
   },
-  components: {
-    Header
+  methods: {
+    async sendParent() {
+      const data = await axios.get(
+      `https://apis.postcode-jp.com/api/v3/postcodes/${this.childText}apiKey=PQF7Paak29Wr688IIVCXC7As7YDPMBGko0KBwdF`);
+      //  data.data[0].allAddress;これで住所のデータを取得できます！
+      const data = "addressData";
+    }
   }
 };
 </script>
