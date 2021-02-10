641e9aac52ee93599b8716b16e86bc773932b7bc
NagahamaTakahiro <nnarannnara27@gmail.com>
2021-02-10 09:58:41 +0900

    message

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,8 @@ export default {
       const data = await axios.get(
       `https://apis.postcode-jp.com/api/v3/postcodes/${this.childText}apiKey=PQF7Paak29Wr688IIVCXC7As7YDPMBGko0KBwdF`);
       //  data.data[0].allAddress;これで住所のデータを取得できます！
-      const data = "addressData";
     }
+    const data = "addressData";
   }
 };
 </script>
